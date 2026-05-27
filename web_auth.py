112d390c57bb29b6a852dfbfb5061fce9918c036
Akerke Oralbek <akerkeoralbek1@gmail.com>
2026-05-27 14:29:55 +0500

    fix oauth flow

--- web_auth.py
+++ web_auth.py
@@ -10,7 +10,15 @@ app = Flask(__name__)
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
-
+CLIENT_CONFIG = {
+    "web": {
+        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+        "client_secret": os.getenv("GOOGLE_CLIENT_SECRET"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "redirect_uris": [REDIRECT_URI],
+    }
+}
 
 @app.route("/")
 def home():
@@ -24,10 +32,10 @@ def auth():
     if not user_id:
         return "Missing user_id", 400
 
-    flow = Flow.from_client_secrets_file(
-        "credentials.json",
-        scopes=SCOPES,
-        redirect_uri=REDIRECT_URI,
+    flow = Flow.from_client_config(
+    CLIENT_CONFIG,
+    scopes=SCOPES,
+    redirect_uri=REDIRECT_URI,
     )
 
     authorization_url, state = flow.authorization_url(
@@ -47,8 +55,8 @@ def oauth2callback():
     if not user_id:
         return "Missing user_id", 400
 
-    flow = Flow.from_client_secrets_file(
-        "credentials.json",
+    from_client_config(
+    CLIENT_CONFIG,flow
         scopes=SCOPES,
         redirect_uri=REDIRECT_URI,
     )
